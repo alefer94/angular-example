a7dcafbceedb40feaa3d69032b6cb94ac5e794b2
alefer94 <afpy1994@gmail.com>
2023-06-23 13:12:51 -0500

    Component EventBinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventBindingComponent } from './Component/event-binding/event-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule,
